4e4b5b2cb0975c21937ec39a1177c3643d6d358f
ankanz <amkan18@gmail.com>
2023-09-07 23:04:03 +0000

    Commit kaniko files Xgboost /provider/Python/SCRDemo

---
+++ Python/Xgboost/unzipped/example_test.py
@@ -0,0 +1,17 @@
+import os
+import os.path
+import sys
+
+sys.path.append('/models/resources/viya/5b2981f2-4814-48cb-825a-463d76801b2c/')
+
+import score_Xgboost
+
+import settings_5b2981f2_4814_48cb_825a_463d76801b2c
+
+settings_5b2981f2_4814_48cb_825a_463d76801b2c.pickle_path = '/models/resources/viya/5b2981f2-4814-48cb-825a-463d76801b2c/'
+
+def score_record(Carrier,DayofMonth,OriginAirport,ScheduledElapsedTimeMinutes,ArrivalHour,DepatureHour):
+    "Output: EM_CLASSIFICATION,EM_EVENTPROBABILITY"
+    return score_Xgboost.score(Carrier,DayofMonth,OriginAirport,ScheduledElapsedTimeMinutes,ArrivalHour,DepatureHour)
+
+print(score_record("",119.54,"",189.11,12.67,44.38))
